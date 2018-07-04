ba0e47f5c35b10658441bbb08d214439d94cec90
huriskayis <huriskayis@gmail.com>
2018-07-04 15:51:58 +0200

    Add files via upload

--- test.js
+++ test.js
@@ -34,37 +34,66 @@ robot_base.position.y = -300;
 robot_base.position.x = 20;
 
 //JOINT-1
-var joint_1_geometry = new THREE.CylinderGeometry(20,20,300);
+var joint_1_geometry = new THREE.CylinderGeometry(15,15,150);
 var joint_1_material = new THREE.MeshBasicMaterial({ color: 0xFF8C00 });
 var joint_1 = new THREE.Mesh(joint_1_geometry,joint_1_material);
-joint_1.position.y = robot_base.position.y +robot_base.geometry.parameters.height /2 + joint_1.geometry.parameters.height /2;
-//joint_1.position.y = 100;
 robot_base.add(joint_1);
+joint_1.position.y = robot_base.geometry.parameters.height /2 + joint_1.geometry.parameters.height /2;
 
 //JOINT-2
-var joint_2_geometry = new THREE.CylinderGeometry(20,20,150);
+var joint_2_geometry = new THREE.CylinderGeometry(15,15,150);
 var joint_2_material = new THREE.MeshBasicMaterial({ color: 0xFF8C00 });
 var joint_2 = new THREE.Mesh(joint_2_geometry,joint_2_material);
-//joint_2.position.y = 200;
+
 joint_1.add(joint_2);
+joint_2.position.y = joint_1.geometry.parameters.height /2 + joint_2.geometry.parameters.height /2;
 
 //JOINT-3
-var joint_3_geometry = new THREE.CylinderGeometry(20,20,150);
+var joint_3_geometry = new THREE.CylinderGeometry(15,15,150);
 var joint_3_material = new THREE.MeshBasicMaterial({ color: 0xFF8C00 });
 var joint_3 = new THREE.Mesh(joint_3_geometry,joint_3_material);
+joint_3.position.y = joint_2.geometry.parameters.height /2 + joint_3.geometry.parameters.height /2;
 joint_2.add(joint_3);
 scene.add(robot_base);
 
-console.log(robot_base.position);
-console.log(joint_1.position);
-console.log(joint_2.position);
-console.log(joint_3.position);
+//FRUITS//
+//ORANGE
+var orange_geometry = new THREE.SphereGeometry(50,50,50);
+var orange_texture = new THREE.TextureLoader().load("orange.jpg");
+var orange_material = new THREE.MeshBasicMaterial({ map: orange_texture });
+orange_texture.wrapS = THREE.RepeatWrapping;
+orange_texture.wrapT = THREE.RepeatWrapping;
+orange_texture.repeat.set( 1, 1);
+var orange = new THREE.Mesh(orange_geometry, orange_material);
+orange.position.z = -1000;
+orange.position.x = -250;
+orange.position.y = 50;
+scene.add(orange);
+//APPLE
+var apple_geometry = new THREE.SphereGeometry(50,50,50);
+var apple_texture = new THREE.TextureLoader().load("apple.jpg");
+var apple_material = new THREE.MeshBasicMaterial({ map: apple_texture });
+apple_texture.wrapS = THREE.RepeatWrapping;
+apple_texture.wrapT = THREE.RepeatWrapping;
+apple_texture.repeat.set( 1, 1);
+var apple = new THREE.Mesh(apple_geometry, apple_material);
+apple.position.z = -1000;
+apple.position.x = 250;
+apple.position.y = 50;
+scene.add(apple);
 
 //////Animation////////
 var animate = function () {
+	orange.rotation.x += 0.01;
+	apple.rotation.y += 0.01;
+	var temp_x = joint_3.position.x - orange.position.x;
+	var temp_y = joint_3.position.y - orange.position.y;
+	ang = Math.atan2(temp_y, temp_x)* 180 / Math.PI;
+	console.log (ang);
+	if (joint_1.rotation.z < ang )
+		joint_1.rotation.z += 0.1;
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
 };
 animate();
-
 /////END//////////////
