e0e3c2c26228dec454ec8d30e4f3d470f5531c88
huriskayis <huriskayis@gmail.com>
2018-07-19 19:24:09 +0200

    Update FruitCollector.js

--- FruitCollector.js
+++ FruitCollector.js
@@ -1,348 +1,222 @@
-"use strict";
+var renderer,
+	scene,
+	camera,
+	light,
+	light2;
 
-var canvas;
-var gl;
-var program;
+var angle_grabber = 0; // grabber_base current angle at the moment
+var button1_check = false;
+var button2_check = false;
+///RENDERER
+renderer = new THREE.WebGLRenderer({canvas: myCanvas, alpha: true });
+renderer.setClearColor(0x000000, 0);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize( window.innerWidth, window.innerHeight);
+document.body.appendChild( renderer.domElement );
 
-var projectionMatrix;
-var modelViewMatrix;
+///CAMERA
+camera = new THREE.PerspectiveCamera( 35, window.innerWidth/window.innerHeight, 0.1, 3000 );
+camera.position.z = 5;
+///SCENE
+scene = new THREE.Scene();
 
-var instanceMatrix;
+///LIGHTS
+light = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(light);
 
-var modelViewMatrixLoc;
+light2 = new THREE.PointLight(0xffffff, 0.5);
+scene.add(light2);
 
-var button =  false; // for animating the robot
-var grabber = false; // for animating the grabber
-var view_3D = true; // 3D
-var angle_3 = 0;
-var angle_grabber = 0;
+//TREEE
+var tree = new THREE.Tree({
+    generations : 4,        // # for branch' hierarchy
+    length      : 4.0,      // length of root branch
+    uvLength    : 16.0,     // uv.v ratio against geometry length (recommended is generations * length)
+    radius      : 0.2,      // radius of root branch
+    radiusSegments : 8,     // # of radius segments for each branch geometry
+    heightSegments : 8      // # of height segments for each branch geometry
+});
 
-var ee_x;
-var ee_y;
+var geometryt = THREE.TreeGeometry.build(tree);
 
-var point_x;
-var point_y;
+var trex = new THREE.Mesh(
+    geometryt, 
+    new THREE.MeshPhongMaterial({}) // set any material
+);
+trex.position.z = -100;
+scene.add(trex);
+//END-OF-TREE///7
 
-var ang;
-var temp_x, temp_y ; 
+//CONSTRUCT THE ROBOT//
+//BASE
+var base_geometry = new THREE.CylinderGeometry(50,50,50);
+var base_material = new THREE.MeshBasicMaterial({ color: 0x31a6ff });
+var robot_base = new THREE.Mesh(base_geometry,base_material);
+robot_base.position.z = -1000;
+robot_base.position.y = -300;
+//robot_base.position.x = 20;
 
-var vertices = [
+//JOINT-1
+var joint_1_geometry = new THREE.CylinderGeometry(10,10,150);
+joint_1_geometry.applyMatrix(new THREE.Matrix4().makeTranslation(0,75,0));
+var joint_1_material = new THREE.MeshBasicMaterial({ color: 0xFF8C00 });
+var joint_1 = new THREE.Mesh(joint_1_geometry,joint_1_material);
+robot_base.add(joint_1);
+joint_1.position.y = robot_base.geometry.parameters.height /2 ;
 
-    vec4( -0.5, -0.5,  0.5, 1.0 ),
-    vec4( -0.5,  0.5,  0.5, 1.0 ),
-    vec4( 0.5,  0.5,  0.5, 1.0 ),
-    vec4( 0.5, -0.5,  0.5, 1.0 ),
-    vec4( -0.5, -0.5, -0.5, 1.0 ),
-    vec4( -0.5,  0.5, -0.5, 1.0 ),
-    vec4( 0.5,  0.5, -0.5, 1.0 ),
-    vec4( 0.5, -0.5, -0.5, 1.0 )
-];
+//JOINT-2
+var joint_2_geometry = new THREE.CylinderGeometry(10,10,150);
+joint_2_geometry.applyMatrix(new THREE.Matrix4().makeTranslation(0,75,0));
+var joint_2_material = new THREE.MeshBasicMaterial({ color: 0x9966cc });
+var joint_2 = new THREE.Mesh(joint_2_geometry,joint_2_material);
 
-var baseId = 0;
-var joint1Id  = 1;
-var joint2Id = 2;
-var joint3Id = 3;
-var gb_Id = 4; // grabber_base_Id
-var gla_Id = 5; // grabber_left_arm_Id
-var gra_Id = 6; // grabber_right_arm_Id
+joint_1.add(joint_2);
+joint_2.position.y = joint_1.geometry.parameters.height;
 
-var baseHeight = 1.0;
-var baseWidth = 3.0;
-var joint1Height = 4.5;
-var joint1Width = 0.25;
-var joint2Height  = 4.5;
-var joint2Width  = 0.25;
-var joint3Height  = 4.5;
-var joint3Width  = 0.25;
-var gb_Height = 0.25;
-var gb_Width = 1.25;
-var ga_Height = 1.25; // grabber_arm_height
-var ga_Width = 0.05; // grabber_arm_width
+//JOINT-3
+var joint_3_geometry = new THREE.CylinderGeometry(10,10,150);
+joint_3_geometry.applyMatrix(new THREE.Matrix4().makeTranslation(0,75,0));
+var joint_3_material = new THREE.MeshBasicMaterial({ color: 0xff6666 });
+var joint_3 = new THREE.Mesh(joint_3_geometry,joint_3_material);
+joint_3.position.y = joint_2.geometry.parameters.height;
+joint_2.add(joint_3);
 
-var square = 1;
+//GRABBER_BASE
+var grabBase_geometry = new THREE.CubeGeometry(100,10,10);
+var grabBase_material = new THREE.MeshBasicMaterial({ color: 0xFF8C00 });
+var grabber_base = new THREE.Mesh(grabBase_geometry, grabBase_material);
+grabber_base.position.y = 150;
+joint_3.add(grabber_base);
 
-var numNodes = 7;
-var numAngles = 4;
-var angle = 0;
+//GRABBER ARMS
+//LEFT_ARM
+var leftArm_geometry = new THREE.CubeGeometry(10,70,10);
+leftArm_geometry.applyMatrix(new THREE.Matrix4().makeTranslation(0,35,0));
+var leftArm_material = new THREE.MeshBasicMaterial({ color: 0xFF8C00 });
+var grabber_leftArm = new THREE.Mesh(leftArm_geometry, leftArm_material);
+grabber_leftArm.position.y = 5;
+grabber_leftArm.position.x = -55;
+//grabber_leftArm.rotation.z = 0.1;
+grabber_base.add(grabber_leftArm);
+//RIGHT_ARM
+var rightArm_geometry = new THREE.CubeGeometry(10,70,10);
+rightArm_geometry.applyMatrix(new THREE.Matrix4().makeTranslation(0,35,0));
+var rightArm_material = new THREE.MeshBasicMaterial({ color: 0xFF8C00 });
+var grabber_rightArm = new THREE.Mesh(rightArm_geometry, rightArm_material);
+grabber_rightArm.position.y = 5;
+grabber_rightArm.position.x = 55;
+//grabber_rightArm.rotation.z = -0.1;
+grabber_base.add(grabber_rightArm);
+scene.add(robot_base);
 
-var theta = [0, 0, 0, 0, 0, 0, 0];
+//FRUITS//
+//ORANGE
+var orange_geometry = new THREE.SphereGeometry(25,25,25);
+var orange_texture = new THREE.TextureLoader().load("orange.jpg");
+var orange_material = new THREE.MeshBasicMaterial({ map: orange_texture });
+orange_texture.wrapS = THREE.RepeatWrapping;
+orange_texture.wrapT = THREE.RepeatWrapping;
+orange_texture.repeat.set( 1, 1);
+var orange = new THREE.Mesh(orange_geometry, orange_material);
+orange.position.z = -1000;
+orange.position.x = -300;
+orange.position.y = 0;
+scene.add(orange);
+//APPLE
+var apple_geometry = new THREE.SphereGeometry(25,25,25);
+var apple_texture = new THREE.TextureLoader().load("apple.jpg");
+var apple_material = new THREE.MeshBasicMaterial({ map: apple_texture });
+apple_texture.wrapS = THREE.RepeatWrapping;
+apple_texture.wrapT = THREE.RepeatWrapping;
+apple_texture.repeat.set( 1, 1);
+var apple = new THREE.Mesh(apple_geometry, apple_material);
+apple.position.z = -1000;
+apple.position.x = 250;
+apple.position.y = 50;
+scene.add(apple);
 
-var numVertices = 24;
+document.getElementById("button1").onclick = function()
+	{button1_check = !button1_check;};
+document.getElementById("button2").onclick = function()
+	{button2_check = !button2_check;};
 
-var stack = [];
-
-var figure = [];
-
-for( var i=0; i<numNodes; i++) figure[i] = createNode(null, null, null, null);
-
-var vBuffer;
-var modelViewLoc;
-
-var pointsArray = [];
-
-//-------------------------------------------
-
-function scale4(a, b, c) {
-   var result = mat4();
-   result[0][0] = a;
-   result[1][1] = b;
-   result[2][2] = c;
-   return result;
+var gp;
+var j3;
+////////////////////////////////////////
+// Function calculates the angle between 3 points, #TODO :: give parameters for later use !!!
+var calculate_angle = function(){
+		gp = grabber_base.getWorldPosition();
+	var op = orange.getWorldPosition();
+	var pb = robot_base.getWorldPosition();
+	var j1 = joint_1.getWorldPosition();
+	var j2 = joint_2.getWorldPosition();
+	    j3 = joint_3.getWorldPosition();
+	/*
+		console.log(pb);
+		console.log(p1);
+		console.log(p2);
+		console.log(p3);
+		console.log(gp);
+		console.log(op);
+	*/
+	// p1 should be origin point = joint 3 pivot
+	var p1={x:j3.x,y:j3.y};
+	var p2={x:gp.x,y:gp.y};
+	var p3={x:op.x,y:op.y};
+	var p12 = Math.sqrt(Math.pow((p1.x - p2.x),2) + Math.pow((p1.y - p2.y),2));
+	var p13 = Math.sqrt(Math.pow((p1.x - p3.x),2) + Math.pow((p1.y - p3.y),2));
+	var p23 = Math.sqrt(Math.pow((p2.x - p3.x),2) + Math.pow((p2.y - p3.y),2));
+	//angle in radians
+	var resultRadian = Math.acos(((Math.pow(p12, 2)) + (Math.pow(p13, 2)) - (Math.pow(p23, 2))) / (2 * p12 * p13));
+	//angle in degrees
+	var resultDegree = Math.acos(((Math.pow(p12, 2)) + (Math.pow(p13, 2)) - (Math.pow(p23, 2))) / (2 * p12 * p13)) * 180 / Math.PI;
+	//console.log(resultRadian);
+	//console.log(resultDegree);
+	resultRadian = Math.round(resultRadian*100) / 100;
+	return resultRadian;
 }
+////////// END OF FUNCTION /////////////////
+///////////////////////////////////////////
 
-//--------------------------------------------
-
-function quad(a, b, c, d) 
+var show_circle = function()
 {
-     pointsArray.push(vertices[a]);
-     pointsArray.push(vertices[b]);
-     pointsArray.push(vertices[c]);
-     pointsArray.push(vertices[d]);
+	//CIRCLESSS
+	gp = grabber_base.getWorldPosition();
+	j3 = joint_3.getWorldPosition();
+	var geometry = new THREE.CircleGeometry( joint_3.geometry.parameters.height+grabber_leftArm.geometry.parameters.height, 32 );
+	var material = new THREE.LineBasicMaterial( { color: 0x020202} );
+	var circle = new THREE.Line( geometry, material );
+
+	circle.position.z = -1000;
+	circle.position.x = j3.x;
+	circle.position.y = j3.y;
+	scene.add( circle );
+
 }
+var resultRadian = calculate_angle();
 
-function cube(){
-    quad( 1, 0, 3, 2 );
-    quad( 2, 3, 7, 6 );
-    quad( 3, 0, 4, 7 );
-    quad( 6, 5, 1, 2 );
-    quad( 4, 5, 6, 7 );
-    quad( 5, 4, 0, 1 );
-}
+//////Animation////////
+var animate = function () {	
+//console.log (resultRadian);
+	if (button2_check)
+		{
+			if (resultRadian != joint_3.rotation.z)
+				{	
+					//console.log(joint_3.rotation.z);
+					joint_3.rotation.z = Math.round(joint_3.rotation.z*100) /100 + 0.01;
+				}	
+			else 
+				show_circle();
+		}
 
-function createNode(transform, render, sibling, child){
-    var node = {
-    transform: transform,
-    render: render,
-    sibling: sibling,
-    child: child,
-    }
-    return node;
-}
-
-function initNodes(Id) 
-{
-
-    var m = mat4();
-
-    switch(Id) {
-
-    case baseId:
-    m = rotate(theta[baseId], 0, 1, 0 );
-    m = mult(translate(0, -8, 0), m);
-    figure[baseId] = createNode( m, base, null, joint1Id );
-    break;
-
-    case joint1Id:
-
-    m = translate(0.0, baseHeight, 0.0);
-	m = mult(m, rotate(theta[joint1Id], 1, 0, 0));
-    figure[joint1Id] = createNode( m, joint1, null, joint2Id);
-    break;
-
-    case joint2Id:
-
-    m = translate(0.0, joint1Height, 0.0);
-	m = mult(m, rotate(theta[joint2Id], 1, 0, 0));
-    figure[joint2Id] = createNode( m, joint2, null, joint3Id );
-    break;
-
-    case joint3Id:
-
-    m = translate(0.0,joint2Height, 0.0);
-	m = mult(m, rotate(theta[joint3Id], 1, 0, 1));
-    figure[joint3Id] = createNode( m, joint3, null, gb_Id );
-    break; 
-    
-    case gb_Id:
-
-    m = translate(0.0,joint3Height, 0.0);
-	m = mult(m, rotate(theta[gb_Id], 0, 0, 1));
-    figure[gb_Id] = createNode( m, grabberBase, null, gla_Id );
-    break;
-
-    case gla_Id:
-
-    m = translate(-gb_Width/2,gb_Height, 0.0);
-	m = mult(m, rotate(theta[gla_Id], 0, 0, 1));
-    figure[gla_Id] = createNode( m, grabberLeftArm, gra_Id, null );
-    break;
-
-    case gra_Id:
-
-    m = translate(gb_Width/2,gb_Height, 0.0);
-	m = mult(m, rotate(theta[gra_Id], 0, 0, 1));
-    figure[gra_Id] = createNode( m, grabberRightArm, null, null );
-    break; 
-
-}}
-
-function traverse(Id) 
-{
-
-   if(Id == null) return;
-   stack.push(modelViewMatrix);
-   modelViewMatrix = mult(modelViewMatrix, figure[Id].transform);
-   figure[Id].render();
-   if(figure[Id].child != null) traverse(figure[Id].child);
-    modelViewMatrix = stack.pop();
-   if(figure[Id].sibling != null) traverse(figure[Id].sibling);
-}
-
-function base() 
-{
-
-    instanceMatrix = mult(modelViewMatrix, translate(0, 0.5*baseHeight, 0.0) );
-    instanceMatrix = mult(instanceMatrix, scale4( baseWidth, baseHeight, baseWidth));
-    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
-    for(var i =0; i<6; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4*i, 4);
-}
-
-function joint1() 
-{
-
-    instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.5 * joint1Height, 0.0 ));
-	instanceMatrix = mult(instanceMatrix, scale4(joint1Width, joint1Height, joint1Width) );
-    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
-    for(var i =0; i<6; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4*i, 4);
-}
-
-function joint2() 
-{
-
-    instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.5 * joint2Height, 0.0) );
-	instanceMatrix = mult(instanceMatrix, scale4(joint2Width, joint2Height, joint2Width) );
-    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
-    for(var i =0; i<6; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4*i, 4);
-}
-
-function joint3() 
-{
-
-    instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.5 * joint3Height, 0.0) );
-	instanceMatrix = mult(instanceMatrix, scale4(joint3Width, joint3Height, joint3Width) );
-    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
-    for(var i =0; i<6; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4*i, 4);
-    ee_x = instanceMatrix [0][3];
-    ee_y = instanceMatrix [1][3];
-}
-
-function grabberBase() 
-{
-
-    instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.5 * gb_Height, 0.0) );
-	instanceMatrix = mult(instanceMatrix, scale4(gb_Width, gb_Height, gb_Width) );
-    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
-    for(var i =0; i<6; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4*i, 4);
-}
-
-function grabberLeftArm() 
-{
-
-    instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.5 * ga_Height, 0.0) );
-	instanceMatrix = mult(instanceMatrix, scale4(ga_Width, ga_Height, ga_Width) );
-    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
-    for(var i =0; i<6; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4*i, 4);
-}
-
-function grabberRightArm() 
-{
-
-    instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.5 * ga_Height, 0.0) );
-	instanceMatrix = mult(instanceMatrix, scale4(ga_Width, ga_Height, ga_Width) );
-    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
-    for(var i =0; i<6; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4*i, 4);
-}
-
-window.onload = function init() 
-{
-
-    canvas = document.getElementById( "myCanvas" );
-
-    gl = WebGLUtils.setupWebGL( canvas );
-    if ( !gl ) { alert( "WebGL isn't available" ); }
-
-    gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor(0.0, 0.0, 0.0, 0.0);
-
-    //
-    //  Load shaders and initialize attribute buffers
-    //
-    program = initShaders( gl, "vertex-shader", "fragment-shader");
-
-    gl.useProgram( program);
-
-    instanceMatrix = mat4();
-    projectionMatrix = ortho(-10.0,10.0,-10.0, 10.0,-10.0,10.0);
-    modelViewMatrix = mat4();
-
-    gl.uniformMatrix4fv(gl.getUniformLocation( program, "modelViewMatrix"), false, flatten(modelViewMatrix) );
-    gl.uniformMatrix4fv( gl.getUniformLocation( program, "projectionMatrix"), false, flatten(projectionMatrix) );
-    modelViewMatrixLoc = gl.getUniformLocation(program, "modelViewMatrix");
-
-    cube();
-
-    vBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
-    
-    var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
-    for(i=0; i<numNodes; i++) initNodes(i);
-
-    
-
-    document.getElementById("Button1").onclick = function()
-    {  button =  !button; };
-    document.getElementById("Button2").onclick = function()
-    {  grabber = !grabber; };
-    document.getElementById("button3D").onclick = function(){view_3D = !view_3D;};
-
-    render();
-}
-
-var render = function() 
-{
-    gl.clear( gl.COLOR_BUFFER_BIT );
-            // projection matrix for 2D and 3D
-    projectionMatrix = ortho(-10.0,10.0,-20.0, 20.0,0.0,50.0);
-        
-            // model view for 2D
-    modelViewMatrix = mat4();
-
-            // model view for 3D
-    if (view_3D)
-    {
-        var eye = vec3(10.0, -10.0, 30.0);
-        const at = vec3(0.0, 1.0, -1.0);
-        const up = vec3(0.0, 5.0, 0.0);
-        modelViewMatrix = lookAt(eye, at, up);      
-    }
-    
-    // To send ModelView info vertex and fragment shaders
-    gl.uniformMatrix4fv(gl.getUniformLocation(program, "modelViewMatrix"), false, flatten(modelViewMatrix));
-    gl.uniformMatrix4fv(gl.getUniformLocation(program, "projectionMatrix"), false, flatten(projectionMatrix));
-    modelViewMatrixLoc = gl.getUniformLocation(program, "modelViewMatrix");
-    temp_x = point_x - ee_x ;
-    temp_y = point_y - ee_y ;
-    ang = Math.atan2(temp_y, temp_x)* 180 / Math.PI;;
-    if (button)
-    {
-        if (theta[3] > ang)
-            angle_3++;
-        theta[3] = angle_3;
-        
-    }
-    if (grabber)
-    {
-        if (theta[gra_Id] < 45 )
-        	angle_grabber++;
-        theta[gla_Id] =  -angle_grabber;
-        theta[gra_Id] =  angle_grabber;
-    }
-
-    for(i=0; i<numNodes; i++) initNodes(i);
-    traverse(baseId);
-    requestAnimFrame(render); 
-    renderer.render( scene, camera );     
-}
+	if (button1_check)
+	{	//console.log("Angle of grabberbase", grabber_rightArm.rotation.z);
+		if (grabber_rightArm.rotation.z < radians(60)){
+			grabber_rightArm.rotation.z += 0.25;
+			grabber_leftArm.rotation.z = -grabber_rightArm.rotation.z;
+		}
+	}	
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+};
+animate();
